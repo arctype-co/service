c5ebddae5f4741d63a8536f2e1bf323676c16a8a
Ryan Sundberg <ryan.sundberg@gmail.com>
2017-07-26 15:50:50 -0700

    Imagemagic resize

--- src/arctype/service/io/im.clj
+++ src/arctype/service/io/im.clj
@@ -1,8 +1,21 @@
 (ns ^{:doc "ImageMagick im4java interface"}
   arctype.service.io.im
   (:import
-    [org.im4java.core Info]))
+    [org.im4java.core ConvertCmd IMOperation Info]))
 
 (defn basic-info
   [path]
   (Info. path true))
+
+; Options: 
+; quality: http://www.imagemagick.org/script/command-line-options.php#quality
+(defn resize! 
+  [in-path out-path w h options]
+  (let [cmd (ConvertCmd.)
+        op (IMOperation.)]
+    (.addImage op (into-array String [(str in-path)]))
+    (.resize op (int w) (int h))
+    (when-let [quality (:quality options)] 
+      (.quality op (double quality)))
+    (.addImage op (into-array String [(str out-path)]))
+    (.run cmd op (into-array Object []))))
