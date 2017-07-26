c5c4dc24112006809fff90d00c148cadca7c2afa
Ryan Sundberg <ryan.sundberg@gmail.com>
2017-07-26 09:57:28 -0700

    Add FFMPEG driver from https://github.com/runexec/ffmpeg-clj

---
+++ src/arctype/service/io/ffmpeg.clj
@@ -0,0 +1,30 @@
+(ns ^{:doc
+      "FFMpeg Driver
+      Derived from https://github.com/runexec/ffmpeg-clj"}
+  arctype.service.io.ffmpeg
+  (:require 
+    [clojure.java.shell :refer [sh]]
+    [clojure.string :as s]))
+
+(def ^:dynamic *bin* "ffmpeg")
+
+(defn cmd [& argv] 
+  (->> argv
+       (map #(if (keyword? %) (str "-" (name %)) (str %)))
+       (into [*bin*])))
+
+(defn bin [] *bin*)
+
+(defn ffmpeg! [& args]
+  (let [cmd! (apply sh (apply cmd args))
+        {:keys [exit out err]} cmd!]
+    (when-not (zero? exit)
+      (throw
+       (Exception. err)))
+      out))
+
+(defn version [] 
+  (as-> (ffmpeg! "-version") o
+    (re-find #"version \S+" o)
+    (s/split o #" ")
+    (last o)))
