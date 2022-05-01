e2c3f8209491b6d379bb18c798d933f8974fb4ca
Ryan Sundberg <ryan@arctype.co>
2022-04-30 21:35:59 -0700

    Requires prismatic/schema 1.2.1-SNAPSHOT custom build

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
    [org.clojure/tools.logging "1.1.0"]
    [commons-codec/commons-codec "1.15"]
    [io.forward/yaml "1.0.10"]
-   [prismatic/schema "1.1.12"]
+   ; Requires branch with S/defprotocol support
+   ; https://github.com/plumatic/schema/pull/432
+   [prismatic/schema "1.2.1-SNAPSHOT"]
    [sundbry/resource "0.4.0"]]
 
   :source-paths ["src"]
